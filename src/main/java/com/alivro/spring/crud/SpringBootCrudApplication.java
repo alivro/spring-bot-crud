2e21258585dfd11f08f55a732c0297be4417be67
alivro <alfonso.rojo.03@outlook.com>
2024-11-24 20:49:06 -0600

    Agrega la anotación @ComponentScan

--- src/main/java/com/alivro/spring/crud/SpringBootCrudApplication.java
+++ src/main/java/com/alivro/spring/crud/SpringBootCrudApplication.java
@@ -2,10 +2,11 @@ package com.alivro.spring.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.alivro.spring.crud"})
 public class SpringBootCrudApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrudApplication.class, args);
 	}
